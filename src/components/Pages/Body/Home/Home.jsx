58d039ecb6d812f923a1b9faab25be0b5cc33bd8
unknown <rialjhe111@gmail.com>
2023-07-22 14:02:50 +0800

    'add:our-service'

--- src/components/Pages/Body/Home/Home.jsx
+++ src/components/Pages/Body/Home/Home.jsx
@@ -6,6 +6,11 @@ import github from "../../../../assets/github.png";
 import linkedin from "../../../../assets/linkedin.png";
 import { NavLink } from "react-router-dom";
 
+// import Our Service
+import webDev from "../../../../assets/web-dev.png";
+import webDesign from "../../../../assets/web-design.png";
+import vidEdit from "../../../../assets/video-editing.png";
+
 export default function Home() {
   return (
     <>
@@ -19,7 +24,7 @@ export default function Home() {
               Front-end <span className="text-[#00E0FF]">Developer</span>
             </h3>
           </div>
-          <p className="text-[16px] xl:leading-loose xl:w-[600px] xl:text-left font-light">
+          <p className="text-[12px] xl:text-[16px] xl:leading-loose xl:w-[600px] xl:text-left font-light">
             I come from Indonesia and live in South Sulawesi, from childhood
             until now I really like to learn all things related to computers
             such as graphic design, programming etc.
@@ -31,7 +36,6 @@ export default function Home() {
             >
               Let's Talk
             </NavLink>
-            
           </div>
           <div className="mt-4 flex gap-1 xl:absolute xl:bottom-[32px]">
             <a
@@ -63,6 +67,42 @@ export default function Home() {
 
         <div className="right-side pt-10 flex justify-center xl:justify-normal xl:pt-0  ">
           <img src={pic} className="xl:w-[28rem] w-[26rem]" />
+        </div>
+      </div>
+      <div className="border-2 px-10 py-6 mb-12 rounded-xl">
+        <h1 className="text-center text-[28px] xl:text-[38px] font-bold">
+          Our <span className="text-[#00E0FF]">Service</span>
+        </h1>
+        <p className="text-center text-[12px] py-4 xl:text-[16px] xl:px-40 font-light">
+          We are a team of dedicated professionals committed to providing
+          top-notch services to our clients.
+        </p>
+        <div className="flex justify-center flex-wrap gap-8">
+          <div className="web-dev px-4 py-6 bg-[#0B2345] xl:w-1/5 text-center">
+            <img src={webDev} alt="" className="pb-4 block mr-auto ml-auto" />
+            <h1>Web Development</h1>
+            <p className="text-[12px] font-light">
+              Lorem ipsum dolor sit, amet consectetur.
+            </p>
+          </div>
+          <div className="web-design px-4 py-6 bg-[#0B2345] xl:w-1/5 text-center">
+            <img
+              src={webDesign}
+              alt=""
+              className="pb-4 block mr-auto ml-auto"
+            />
+            <h1>Web Design</h1>
+            <p className="text-[12px] font-light">
+              Lorem ipsum dolor sit, amet consectetur.
+            </p>
+          </div>
+          <div className="video-editing px-4 py-6 bg-[#0B2345] xl:w-1/5 text-center">
+            <img src={vidEdit} alt="" className="pb-4 block mr-auto ml-auto" />
+            <h1>Video Editing</h1>
+            <p className="text-[12px] font-light">
+              Lorem ipsum dolor sit, amet consectetur.
+            </p>
+          </div>
         </div>
       </div>
     </>
