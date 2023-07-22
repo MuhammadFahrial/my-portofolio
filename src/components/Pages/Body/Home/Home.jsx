93cad4b85a1c634b2866b583c67b411f1c28ce76
unknown <rialjhe111@gmail.com>
2023-07-22 21:46:35 +0800

    'update:services'

--- src/components/Pages/Body/Home/Home.jsx
+++ src/components/Pages/Body/Home/Home.jsx
@@ -78,30 +78,26 @@ export default function Home() {
           to my clients.
         </p>
         <div className="flex justify-center flex-wrap gap-8">
-          <div className="web-dev px-4 py-6 bg-[#0B2345] xl:w-1/5">
+          <div className="web-dev px-10 py-6 bg-[#0B2345] xl:w-1/5">
             <img src={webDev} alt="" className="pb-4" />
-            <h1>Web Development</h1>
-            <p className="text-[14px] font-light">
+            <h1 className="text-[18px]">Web Development</h1>
+            <p className="text-[14px] font-light pt-2">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo
               error odio optio.
             </p>
           </div>
-          <div className="web-design px-4 py-6 bg-[#0B2345] xl:w-1/5 text-center">
-            <img
-              src={webDesign}
-              alt=""
-              className="pb-4 block mr-auto ml-auto"
-            />
-            <h1>Web Design</h1>
-            <p className="text-[14px] font-light">
+          <div className="web-design px-10 py-6 bg-[#0B2345] xl:w-1/5">
+            <img src={webDesign} alt="" className="pb-4" />
+            <h1 className="text-[18px]">Web Design</h1>
+            <p className="text-[14px] font-light pt-2">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo
               error odio optio.
             </p>
           </div>
-          <div className="video-editing px-4 py-6 bg-[#0B2345] xl:w-1/5 text-center">
-            <img src={vidEdit} alt="" className="pb-4 block mr-auto ml-auto" />
-            <h1>Video Editing</h1>
-            <p className="text-[14px] font-light">
+          <div className="video-editing px-10 py-6 bg-[#0B2345] xl:w-1/5">
+            <img src={vidEdit} alt="" className="pb-4" />
+            <h1 className="text-[18px]">Video Editing</h1>
+            <p className="text-[14px] font-light pt-2">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo
               error odio optio.
             </p>
