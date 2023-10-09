82763eede850bdf3c2c7cf22412b3cbf7021651e
unknown <rialjhe111@gmail.com>
2023-10-09 11:06:59 +0800

    update:HomePage

--- src/components/Pages/Body/Home/Home.jsx
+++ src/components/Pages/Body/Home/Home.jsx
@@ -213,7 +213,7 @@ export default function Home() {
             </p>
             <NavLink
               className=" bg-[#00E0FF] text-black px-4 py-2 font-normal rounded-[24px] text-[16px] inline-block mt-4"
-              to="/portofolio/ui/ux"
+              to="/portofolio/motion-graphic"
             >
               Get Started
             </NavLink>
